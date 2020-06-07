eba9f24042f8b067307d4e1b71604e7822b160f9
Cattarsis <formalkane@mail.ru>
2020-06-07 18:56:43 +0400

    Исправления: форматирование, синтаксис, стили именования, переменные. Вертикальное распределение пинов, адрес по location, функция с min max, shuffle массива, сдвиг координат пина теперь в константах.

--- js/main.js
+++ js/main.js
@@ -1,98 +1,116 @@
 'use strict';
-let HOUSE_TYPE = ['place', 'flat', 'house', 'bungalo'];
-let TIMES = ['12:00', '13:00', '14:00'];
-let FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-let PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+var HOUSE_TYPE = ['place', 'flat', 'house', 'bungalo'];
+var TIMES = ['12:00', '13:00', '14:00'];
+var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
-]
+];
+var MIN_Y = 130;
+var MAX_Y = 630;
 
-let randomInt = function(num) {
+var PIN_OFFSET_X = 25;
+var PIN_OFFSET_Y = 70;
+
+var MAX_PRICE = 3500;
+var MAX_ROOMS = 5;
+
+var IMG_CNT_LEN = 2;
+
+var randomInt = function (num) {
   return Math.floor(Math.random() * num);
-}
+};
 
-let getRandomArray = function(array) {
-  let len = randomInt(array.length + 1);
-  let newArray = new Set();
+var random = function (min, max) {
+  return min + Math.floor(Math.random() * (max - min));
+};
 
-  while(newArray.size < len) {
-    newArray.add(array[randomInt(array.length)]);
+var shuffle = function (array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = randomInt(i + 1);
+    var tmp = array[i];
+    array[i] = array[j];
+    array[j] = tmp;
   }
+};
 
-  return Array.from(newArray);
-}
+var getRandomArray = function (array) {
+  var len = randomInt(array.length + 1);
+  var newArray = array.slice();
+  shuffle(newArray);
 
-let createAdverts = function (cnt, xMin, xMax) {
-  let adverts = [];
+  return newArray.slice(0, len);
+};
 
-  for (let i = 0; i < cnt; i++) {
-    let num = `${i + 1}`.padStart(2,'0');
-    let x = randomInt(1000);
-    let y = randomInt(1000);
-    let rooms = 1 + randomInt(5);
-    let guests = randomInt(rooms);
 
-    let element = {
+var createAdverts = function (cnt, xMin, xMax) {
+  var adverts = [];
+
+  for (var i = 0; i < cnt; i++) {
+    var num = ((i + 1) + '').padStart(IMG_CNT_LEN, '0');
+    var x = random(xMin, xMax);
+    var y = random(MIN_Y, MAX_Y);
+    var rooms = random(1, MAX_ROOMS);
+    var guests = random(0, rooms);
+
+    var element = {
       'author': {
-        'avatar': `img/avatars/user${num}.png`,
+        'avatar': 'img/avatars/user' + num + '.png',
       },
       'offer': {
-          'title': 'строка, заголовок предложения',
-          'address': `${x}, ${y}`,
-          'price': randomInt(3500),
-          'type': HOUSE_TYPE[randomInt(HOUSE_TYPE.length)],
-          'rooms': rooms,
-          'guests': guests,
-          'checkin': TIMES[randomInt(TIMES.length)],
-          'checkout': TIMES[randomInt(TIMES.length)],
-          'features': getRandomArray(FEATURES),
-          'description': 'строка с описанием',
-          'photos': getRandomArray(PHOTOS),
+        'title': 'строка, заголовок предложения',
+        'address': x + ', ' + y,
+        'price': randomInt(MAX_PRICE),
+        'type': HOUSE_TYPE[randomInt(HOUSE_TYPE.length)],
+        'rooms': rooms,
+        'guests': guests,
+        'checkin': TIMES[randomInt(TIMES.length)],
+        'checkout': TIMES[randomInt(TIMES.length)],
+        'features': getRandomArray(FEATURES),
+        'description': 'строка с описанием',
+        'photos': getRandomArray(PHOTOS),
       },
       'location': {
-          'x': xMin + randomInt(xMax+1),
-          'y': 130 + randomInt(630)
+        'x': x,
+        'y': y
       }
-
-    }
+    };
     adverts.push(element);
   }
 
   return adverts;
-}
+};
 
-let createPin = function(pinData) {
-  let pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-  let pin = pinTemplate.cloneNode(true);
+var createPin = function (pinData) {
+  var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+  var pin = pinTemplate.cloneNode(true);
 
   pin.querySelector('img').src = pinData.author.avatar;
   pin.querySelector('img').alt = pinData.offer.title;
 
-  //не получается получить размер элемента, пока его нет в разметке
-  pin.style.left = pinData.location.x - 25 + "px";
-  pin.style.top = pinData.location.y - 70 + "px";
+  pin.style.left = pinData.location.x - PIN_OFFSET_X + 'px';
+  pin.style.top = pinData.location.y - PIN_OFFSET_Y + 'px';
 
   return pin;
-}
+};
 
 
-let createPinFragment = function(pins){
-  let fragment = document.createDocumentFragment();
-  for (const pin of pins) {
+var createPinFragment = function (pins) {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < pins.length; i++) {
+    var pin = pins[i];
     fragment.appendChild(createPin(pin));
   }
   return fragment;
-}
+};
 
-let map = document.querySelector('.map');
+var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 
-let adverts = createAdverts(8,0,map.offsetWidth);
+var adverts = createAdverts(8, 0, map.offsetWidth);
 
-let pinsFragment = createPinFragment(adverts);
+var pinsFragment = createPinFragment(adverts);
 
-let pins = document.querySelector('.map__pins');
+var pins = document.querySelector('.map__pins');
 pins.appendChild(pinsFragment);
-
-
-
